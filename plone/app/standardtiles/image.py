51a997b850dc4d29fdd9ff719dfb3427594efc35
Israel Saeta Pérez <dukebody@gmail.com>
2010-06-18 09:37:46 +0000

    Make the image tile use images already existing on the site.
    svn path=/sandbox/plone.app.standardtiles/branches/dukebody-imagetile-repo/; revision=37347

--- plone/app/standardtiles/image.py
+++ plone/app/standardtiles/image.py
@@ -1,64 +1,45 @@
+from zope.interface import directlyProvides
 from zope import schema
-from zope.schema import getFields
-from zope.interface import implements, Interface
-from zope.component import adapts
+from zope.schema.vocabulary import SimpleVocabulary
+from zope.schema.interfaces import IVocabularyFactory
 from zope.publisher.interfaces import NotFound
 from zope.publisher.interfaces import IPublishTraverse
+from zope.app.component.hooks import getSite
 
 from plone.directives import form as directivesform
 
-from plone.tiles import PersistentTile
-from plone.tiles.interfaces import ITileDataManager
-from plone.tiles.data import PersistentTileDataManager
+from plone.tiles import Tile
 
 from plone.namedfile.utils import set_headers, stream_data
-from plone.namedfile.field import NamedImage as NamedImageField
-from plone.namedfile import NamedImage
 
-from persistent.dict import PersistentDict
+from Products.CMFCore.utils import getToolByName
+from Products.ATContentTypes.interfaces import IATImage
 
 
-class IImageTileType(Interface):
-    pass
+def availableImagesVocabulary(context):
+    """Vocabulary composed of ATImages inside the '/images' folder
+    of the site root.
+    """
+    site = getSite()  # the real context is lost somewhat
+    catalog = getToolByName(site, 'portal_catalog')
+    portal_state = site.restrictedTraverse('@@plone_portal_state')
+    root_path = portal_state.navigation_root_path()
+    images_path = root_path + '/images'
+    results = catalog(path=images_path,
+                      object_provides=IATImage.__identifier__)
+    items = [(r.id, r.id) for r in results]
+    return SimpleVocabulary.fromItems(items)
+directlyProvides(availableImagesVocabulary, IVocabularyFactory)
 
 
 class IImageTile(directivesform.Schema):
 
-    image = NamedImageField(title=u"Please upload an image", required=True)
+    imageUID = schema.Choice(title=u"Image UID", required=True,
+                             vocabulary="Available Images")
     altText = schema.TextLine(title=u"Alternative text", required=False)
 
 
-class ImageTileDataManager(PersistentTileDataManager):
-    """A data manager for the image tile. It stores the image in the '/images'
-    folder of the portal, and the altText in the attributes.
-    """
-
-    implements(ITileDataManager)
-    adapts(IImageTileType)
-
-    def get(self):
-        data = dict(self.annotations.get(self.key, {}))
-        if self.tileType is not None and self.tileType.schema is not None:
-            for name, field in getFields(self.tileType.schema).items():
-                if name not in data:
-                    data[name] = field.missing_value
-        filename = data['image_name']
-        atimage = getattr(self.context.images, filename, None)
-        if atimage is not None:
-            data['image'] = NamedImage(atimage.getImage().data, filename=filename)
-        return data
-
-    def set(self, data):
-        image = data['image']
-        repo = self.context.images
-        repo.invokeFactory('Image', image.filename)
-        atimage = getattr(repo, image.filename)
-        atimage.setImage(image.data)
-        data['image_name'] = image.filename
-        self.annotations[self.key] = PersistentDict(data)
-
-
-class ImageTile(PersistentTile):
+class ImageTile(Tile):
     """Image tile.
     
     This is a persistent tile which stores an image and optionally alt
@@ -73,18 +54,20 @@ class ImageTile(PersistentTile):
     access its persistent data.
     """
 
-    implements(IImageTileType)
-
     def __call__(self):
         # Not for production use - this should be in a template!
-        image = self.data.get('image', None)
+        imageUID = self.data.get('imageUID')
+        image = getattr(self.context.images, imageUID, None)
         if image is not None:
-            altText = self.data.get('altText', '').replace('"', '\"')
-            filename = image.filename
-            imageURL = "%s/@@download/%s" % (self.url, filename,)
+            altText = self.data.get('altText')
+            if altText is not None:
+                altText = altText.replace('"', '\"')
+            else:
+                altText = image.Title()
+            imageURL = image.absolute_url() + '/image'
             return '<html><body><img src="%s" alt="%s" /></body></html>' % (imageURL, altText)
         else:
-            return '<html><body><em>No image set</em></body></html>'
+            return '<html><body><em>Image not found.</em></body></html>'
 
 class ImageTileDownload(object):
     """Implementation of the @@download view on the image tile.
@@ -114,4 +97,3 @@ class ImageTileDownload(object):
         
         set_headers(image, self.request.response, filename=self.filename)
         return stream_data(image)
-
