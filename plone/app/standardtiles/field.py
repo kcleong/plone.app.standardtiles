12393168ef44f9e5f7ca282ce5db29292062a6dd
Israel Saeta Pérez <dukebody@gmail.com>
2010-08-03 12:49:47 +0000

    Play only with the field we're interested in.
    svn path=/sandbox/plone.app.standardtiles/branches/dukebody-fieldtiles/; revision=38481

--- plone/app/standardtiles/field.py
+++ plone/app/standardtiles/field.py
@@ -2,7 +2,6 @@ from z3c.form.form import DisplayForm
 from z3c.form.field import Fields
 from plone.dexterity.utils import iterSchemata
 from plone.tiles import Tile
-from plone.supermodel.utils import mergedTaggedValueDict
 from plone.autoform.interfaces import WIDGETS_KEY
 from plone.autoform.utils import resolveDottedName
 from plone.z3cform import z2
@@ -15,16 +14,25 @@ class DexterityFieldTile(DisplayForm, Tile):
     def __init__(self, context, request):
         Tile.__init__(self, context, request)
         DisplayForm.__init__(self, context, request)
-        self.fields = Fields(*iterSchemata(self.context))
+        self.fieldname = self.data['field']
+        
+        for schema in iterSchemata(self.context):
+            if self.fieldname in schema:
+                # XXX: will this work if there's more than one fields
+                # with the same name in different schemas? One should
+                # be able to use interface.fieldname instead.
+                self.schema = schema
+                self.fields = Fields(schema[self.fieldname])
+                return
+
+        raise KeyError('Field with name %s not found.' % self.fieldname)
 
     def updateWidgets(self):
-        for schema in iterSchemata(self.context):
-            widgets = mergedTaggedValueDict(schema, WIDGETS_KEY)
-            for name, factory in widgets.items():
-                if self.fields[name].widgetFactory.get(self.mode, None) is None:
-                    if isinstance(factory, basestring):
-                        factory = resolveDottedName(factory)
-                    self.fields[name].widgetFactory = factory
+        factory = self.schema.getTaggedValue(WIDGETS_KEY)[self.fieldname]
+        if self.fields[self.fieldname].widgetFactory.get(self.mode, None) is None:
+            if isinstance(factory, basestring):
+                factory = resolveDottedName(factory)
+            self.fields[self.fieldname].widgetFactory = factory
         DisplayForm.updateWidgets(self)
 
     def _wrap_widget(self, render):
@@ -33,4 +41,4 @@ class DexterityFieldTile(DisplayForm, Tile):
     def __call__(self):
         z2.switch_on(self)
         self.update()
-        return self._wrap_widget(self.widgets[self.data['field']].render())
+        return self._wrap_widget(self.widgets[self.fieldname].render())
