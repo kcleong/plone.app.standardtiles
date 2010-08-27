3c04808c5a7faaeae23ce1d956cfcb6e3f998846
Simone Deponti <shywolf9982@gmail.com>
2010-08-27 16:06:46 +0000

    Field tile now takes field permissions into consideration. To be optimized.
    svn path=/sandbox/plone.app.standardtiles/branches/10910-fixregistry/; revision=39090

--- plone/app/standardtiles/field.py
+++ plone/app/standardtiles/field.py
@@ -3,8 +3,8 @@ from z3c.form.field import Fields
 from plone.dexterity.utils import iterSchemata
 from plone.tiles import Tile
 from plone.supermodel.utils import mergedTaggedValueDict
-from plone.autoform.interfaces import WIDGETS_KEY
-from plone.autoform.utils import resolveDottedName
+from plone.autoform.interfaces import WIDGETS_KEY, READ_PERMISSIONS_KEY
+from plone.autoform.utils import _getDisallowedFields, resolveDottedName
 from plone.z3cform import z2
 
 
@@ -24,16 +24,35 @@ class DexterityFieldTile(DisplayForm, Tile):
                 name = self.data['field']
                 if name in widgets:
                     factory = widgets[name]
-                    if self.fields[name].widgetFactory.get(self.mode, None) is None:
+                    if self.fields[name].widgetFactory.get(
+                                self.mode, None) is None:
                         if isinstance(factory, basestring):
                             factory = resolveDottedName(factory)
                         self.fields[name].widgetFactory = factory
         DisplayForm.updateWidgets(self)
+
+    @property
+    def isVisible(self):
+        """Checks wheter the user has read permission of the field: if this is
+        not the case, then the field is not displayed
+        """
+        for schema in iterSchemata(self.context):
+            if self.data['field'] in schema:
+                if self.data['field'] in _getDisallowedFields(
+                    self.context,
+                    mergedTaggedValueDict(schema, READ_PERMISSIONS_KEY),
+                    ''
+                ):
+                    return False
+        return True
 
     def _wrap_widget(self, render):
         return u"<html><body>%s</body></html>" % render
 
     def __call__(self):
         z2.switch_on(self)
-        self.update()
-        return self._wrap_widget(self.widgets[self.data['field']].render())
+        if self.isVisible:
+            self.update()
+            return self._wrap_widget(self.widgets[self.data['field']].render())
+        else:
+            return self._wrap_widget(u'')
